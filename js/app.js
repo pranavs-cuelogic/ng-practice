eef3a73afc944353b9fbb62e5fa3babd556e3f18
vijithv-cuelogic <vijith.vijayan@cuelogic.co.in>
2014-08-06 18:31:35 +0530

    assignment#2 & #3 - partials views using route and common header/footer using directive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,50 @@
-angular.module('myApp', [])
-	.controller('SampleController', function($scope){
+angular.module('myApp', ['ngRoute'])
+	.controller('HomeController', function($scope){
 		$scope.welcomeText = "Hello World!";
 	})
+	.controller('AboutController', function($scope){
+		$scope.message = "About Me";
+	})
+	.controller('ClientController', function($scope){
+		$scope.message = "Our Clients";
+	})
+	.controller('CareerController', function($scope){
+		$scope.message = "Careers";
+	})
+	.controller('TocController', function($scope){
+		$scope.message = "TOC list";
+	})
+	.directive('myheader', function () {
+	    return {
+	        templateUrl: "partials/directives/header.html"
+	    }
+	})
+	.directive('myfooter', function () {
+	    return {
+	        templateUrl: "partials/directives/footer.html"
+	    }
+	})
+	.config(function($routeProvider){
+		$routeProvider
+			.when('/', {
+				controller : 'HomeController',
+				templateUrl : 'partials/home.html'
+			})
+			.when('/about-us', {
+				controller : 'AboutController',
+				templateUrl : 'partials/about.html'
+			})
+			.when('/join-us', {
+				controller : 'CareerController',
+				templateUrl : 'partials/career.html'
+			})
+			.when('/cuelogic/clients', {
+				controller : 'ClientController',
+				templateUrl : 'partials/client.html'
+			})
+			.when('/terms-­and-­conditions', {
+				controller : 'TocController',
+				templateUrl : 'partials/toc.html'
+			})
+	})
+	;
